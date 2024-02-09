9637d2bb931919e455d4d31464551f5785206981
Shannen Saez <shannen@dvslabs.dev>
2024-02-10 03:36:58 +1100

    implement a dynamic svelte island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://vermillion-cat-eb9dcc.netlify.app"
+  site: "https://vermillion-cat-eb9dcc.netlify.app",
+  integrations: [svelte()]
 });
---
+++ src/components/Greeting.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let messages;
+  let greeting = messages[0];
+
+  function randomMessage() {
+    greeting = messages[Math.floor(Math.random() * messages.length)];
+  }
+</script>
+
+<div>
+  <h3>{greeting}! Thank you for visiting!</h3>
+  <button on:click={randomMessage}> New Greeting </button>
+</div>
